3ef69b5cd1cc6c7f808c1a2426beb6e86eba6d25
Karin0909 <uchiha.karin09@gmail.com>
2025-07-17 21:59:15 +0300

    service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,15 +1,17 @@
-const CACHE_NAME = 'origami-world-v1';
+const CACHE_NAME = 'origami-cache-v3';
 const urlsToCache = [
-  './',                // Главная страница
-  './index.html',      // index.html
-  './history.html',    // history.html
-  './schemes.html',    // schemes.html
-  './order.html',      // order.html
-  './contacts.html',   // contacts.html
-  './style.css',       // CSS-файл
-  './images/icon-192.png', // Иконки
+  './',
+  './index.html',
+  './history.html',
+  './schemes.html',
+  './order.html',
+  './contacts.html',
+  './style.css',
+  './images/icon-192.png',
   './images/icon-512.png',
-  // Остальные важные файлы (JS, изображения)
+  './images/easy.png',
+  './images/medium.jpg',
+  './images/hurd.jpg'
 ];
 
 self.addEventListener('install', (event) => {
@@ -23,6 +25,6 @@ self.addEventListener('fetch', (event) => {
   event.respondWith(
     caches.match(event.request)
       .then((response) => response || fetch(event.request))
-      .catch(() => caches.match('./index.html')) // Fallback, если страница не в кэше
+      .catch(() => caches.match('./index.html'))
   );
 });
